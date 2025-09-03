cc165a8cd931790ca347fbc051eea38979afdf89
Vinicius Aguiar <vini1068@gmail.com>
2025-09-02 21:42:15 -0300

    feat: Adicionado testes automatizados

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -14,7 +14,7 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 
     @Query("""
-            select m from Medico m 
+            select m from Medico m
             where 
             m.ativo = true
             and 
